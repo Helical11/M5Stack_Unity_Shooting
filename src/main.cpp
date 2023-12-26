99ca8d4e46bfd2418806669357c218fefacff995
Helical <helical@helical-engineer.com>
2023-12-26 21:58:29 +0900

    [Add]バッテリー残量のタイマー割り込み確認機能

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,10 @@ volatile int WaitingTimer = 1000;
 volatile bool interruptFlag = false;
 //! 最後に割り込みが実施された時間
 volatile uint64_t lastInterruptTime = 0;
-;
+//! タイマー割り込み許可フラグ
+volatile bool timerFlag = false;
+//! タイマー
+hw_timer_t* timer = NULL;
 
 /**
  * @brief トリガー割り込み関数
@@ -41,6 +44,13 @@ void IRAM_ATTR onInterrupt() {
         interruptFlag = true;  // 割り込み許可フラグをたてる
         lastInterruptTime = currentTime;  // 最後の割り込み実施時間を更新
     }
+}
+
+/**
+ * @brief タイマー0の割り込み
+ */
+void IRAM_ATTR onTimer0() {  // タイマー割り込みで呼ばれる関数
+    timerFlag = true;
 }
 
 /**
@@ -58,19 +68,30 @@ void Display_Power(uint8_t status) {
 
 /**
  * @brief M5StickCのバッテリー残量判定
- * @param status バッテリー残量電圧[V]
  */
-void Display_Battery(uint8_t status) {
-    if (status > 4) status = 4;
+void Display_Battery() {
+    float voltage = M5.Power.getBatteryVoltage();  // バッテリー残量取得
+    int batterylevel = map(voltage, 3.2, 4.2, 0, 4);  // 20%毎に変換
+    batterylevel = constrain(batterylevel, 0, 4);  // 4以上にならないよう制限
+
+    // バッテリー外枠描画
+    M5.Display.fillRect(119, 4, 14, 28, WHITE);
+    M5.Display.fillRect(123, 2, 6, 2, WHITE);
     M5.Display.fillRect(121, 6, 10, 24, BLACK);
-    if (status) {
+    M5.Display.fillRect(121, 6, 10, 24, BLACK);
+
+    // バッテリーレベルに合わせて描画、0の場合は赤表示
+    if (batterylevel) {
         M5.Display.fillRect(121, 26, 10, 4, DARKGREEN);
-        for (int i = 0; i < status; i++) {
+        for (int i = 0; i < batterylevel; i++) {
             M5.Display.fillRect(121, 21 - i * 5, 10, 5, DARKGREEN);
         }
     } else {
         M5.Display.fillRect(121, 26, 10, 4, RED);
     }
+
+    // シリアルデバッグ
+    Serial.println(batterylevel);
 }
 
 /**
@@ -78,11 +99,8 @@ void Display_Battery(uint8_t status) {
  */
 void Display_Ini() {
     M5.Display.fillScreen(BLACK);
-    M5.Display.fillRect(119, 4, 14, 28, WHITE);
-    M5.Display.fillRect(123, 2, 6, 2, WHITE);
-    M5.Display.fillRect(121, 6, 10, 24, BLACK);
 
-    Display_Battery(2);
+    Display_Battery();
     Display_Power(1);
 }
 
@@ -111,6 +129,14 @@ void setup() {
     Serial.begin(115200);
     bts.begin("GunController");
 
+    //----------------------------------------
+    // タイマー割り込み初期化(/60s)
+    //----------------------------------------
+    timer = timerBegin(0, 80, true);
+    timerAttachInterrupt(timer, &onTimer0, true);
+    timerAlarmWrite(timer, 60000000, true);
+    timerAlarmEnable(timer);
+
     delay(500);
 }
 
@@ -124,10 +150,19 @@ void loop() {
     //----------------------------------------
     // トリガーを押した場合の処理
     //----------------------------------------
-    if (interruptFlag) {        // トリガー割り込みが発生した場合
-        Serial.println("Yes");  // シリアル送信
-        bts.println("1");       // Bluetooth送信
+    if (interruptFlag) {  // トリガー割り込みが発生した場合
+        Serial.println("trigerInterrupt");  // シリアル送信
+        bts.println("1");                   // Bluetooth送信
         interruptFlag = false;  // 割り込み許可フラグをさげる
+    }
+
+    //----------------------------------------
+    // タイマー割り込みがかかった場合の処理
+    //----------------------------------------
+    if (timerFlag) {  // タイマー割り込みが発生した場合
+        Serial.println("timerInterrupt");  // シリアル送信
+        Display_Battery();
+        timerFlag = false;  // タイマー割り込みフラグをさげる
     }
 
     //----------------------------------------
